940fc20cdc3994f8c8aff01959bdaa954026f30b
Alexander <alexander.wnnr@gmail.com>
2021-01-09 12:54:29 +0200

    add new

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const config = require('config')
 const PORT = process.env.PORT || 5000
+const mongoURI = config.get('mongoURI')
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send('API Running')
-})
-app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`)
-})
+
+async function start() {
+    try {
+        await mongoose.connect(mongoURI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+        console.log('Connected to database...')
+    } catch (error) {
+        console.error(error.message)
+        process.exit(1)
+    }
+    app.use('/api/users', require('./routes/api/users'))
+    app.use('/api/profile', require('./routes/api/profile'))
+    app.use('/api/posts', require('./routes/api/posts'))
+    app.use('/api/auth', require('./routes/api/auth'))
+
+    app.listen(PORT, () => {
+        console.log(`Server started on port ${PORT}`)
+    })
+}
+start()
